958f6bac27302c8f41ec8434bfe458f105ae5905
deepika.kangne <deepika.kangne2095@gmail.com>
2025-09-21 02:48:14 +0530

    HTML

--- ofc_emp_proj/ofc_attendance_app/urls.py
+++ ofc_emp_proj/ofc_attendance_app/urls.py
@@ -4,5 +4,14 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about.html', views.about, name='about'),              # About
+    path('team.html', views.team, name='team'),                 # Team (under About dropdown)
+    path('testimonials.html', views.testimonials, name='testimonials'),  # Testimonials (under About dropdown)
+    path('services.html', views.services, name='services'),     # Services
+    # path('services.html', views.services()),
+    path('portfolio.html', views.portfolio, name='portfolio'),  # Portfolio
+    path('pricing.html', views.pricing, name='pricing'),        # Pricing
+    path('blog.html', views.blog, name='blog'),                 # Blog
+    path('contact.html', views.contact, name='contact'),        # Contact
 ]
--- ofc_emp_proj/ofc_attendance_app/views.py
+++ ofc_emp_proj/ofc_attendance_app/views.py
@@ -4,3 +4,28 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
+def services(request):
+    print("In services view!")
+    return render(request, 'services.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
