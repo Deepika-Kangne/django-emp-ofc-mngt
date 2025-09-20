0447fd6ccfd27d477d9141fea024da8481080958
deepika.kangne <deepika.kangne2095@gmail.com>
2025-09-21 01:26:03 +0530

    Statis

--- ofc_emp_proj/ofc_emp_proj/settings.py
+++ ofc_emp_proj/ofc_emp_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&bka8^5deqvedg14%_9^09*s4d^2s)w9qyebd#jfkt8$rkh$#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-emp-ofc-mngt.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
